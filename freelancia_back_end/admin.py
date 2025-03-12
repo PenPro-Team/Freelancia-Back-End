1ccc237c139be340b2c8b8ca9ccc29f8a3036270
AbdAlla-AboElMagd <abd.moh1990@gmail.com>
2025-03-12 03:21:17 +0200

    Initialize the Project

---
+++ freelancia_back_end/admin.py
@@ -0,0 +1,28 @@
+from django.contrib import admin
+
+from django.contrib.auth.admin import UserAdmin
+
+from freelancia_back_end.models import Project, Proposal, Skill, User
+
+# Override UserAdmin to add custom fields
+class CustomUserAdmin(UserAdmin):
+    list_display = ( 'id' , 'username' , 'email', 'role' , 'first_name', 'last_name', 'is_staff', 'is_superuser')
+    search_fields = ( 'id', 'username', 'email', 'first_name', 'last_name')
+    ordering = ('id',)
+    fieldsets = (
+        (None, {'fields': ( 'username' , 'email', 'password')}),
+        ('Personal info', {'fields': ('first_name', 'last_name', 'phone', 'image')}),
+        ('Permissions', {'fields': ( 'role' , 'is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ( 'username' , 'email', 'first_name' , 'last_name' , 'password1', 'password2','is_staff', 'is_superuser'),
+        }),
+    )
+
+# Register your models here.
+admin.site.register(User, CustomUserAdmin)
+admin.site.register(Skill)
+admin.site.register(Project)
+admin.site.register(Proposal)
---
+++ freelancia_back_end/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class FreelanciaBackEndConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'freelancia_back_end'

dc0bf5af2fe5c505113a30220215177d955007da
AbdAlla-AboElMagd <abd.moh1990@gmail.com>
2025-03-14 19:13:37 +0200

    Editing URLS of endpoints

--- freelancia_back_end/urls.py
+++ freelancia_back_end/urls.py
@@ -7,27 +7,27 @@ urlpatterns = [
     # Proposal Read (get) API
     path('proposals/', proposal_list, name='proposal_list'),
     path('proposals/<int:id>', proposal_detail, name='proposal_detail'),
-    path('proposal/user/<int:id>', proposal_by_user, name='user_proposals'),
-    path('proposal/project/<int:id>',proposal_by_project, name='project_proposals'),
+    path('proposals/user/<int:id>', proposal_by_user, name='user_proposals'),
+    path('proposals/project/<int:id>',proposal_by_project, name='project_proposals'),
     # Proposal Post API
-    path('proposal/', ProposalAPI.as_view(), name='proposal_api_create'),
+    path('proposals/create', ProposalAPI.as_view(), name='proposal_api_create'),
     # Prpoposal READ Write GET
-    path('proposal/<int:id>', ProposalAPI.as_view(), name='proposal_api'),
+    path('proposals/<int:id>', ProposalAPI.as_view(), name='proposal_api'),
     # view For User Oprations
-    path('api/v1/user/', userView, name='userView'),
-    path('api/v1/user/<int:pk>', views.userDetailView),
+    path('users/', userView, name='userView'),
+    path('users/<int:pk>', views.userDetailView),
     # Project Urls Handling
     # List view for all projects
     path('projects/', ProjectView, name='project_list'),
     path('projects/search/', ProjectSearchFilterView.as_view(), name="projects-search-filter"),
     # API endpoint for create operation
-    path('project/', ProjectAPI.as_view(), name='project_api_create'),
+    path('projects/create/', ProjectAPI.as_view(), name='project_api_create'),
     # API endpoint for update (PUT/PATCH) and delete operations on a specific project
-    path('project/<int:id>', ProjectAPI.as_view(), name='project_api'),
+    path('projects/<int:id>', ProjectAPI.as_view(), name='project_api'),
     # Api Gets All the Skills
     path('skills/', skill_list, name='skill_list'),
     # Api Get Push and Delete the Skill
-    path('skill/<int:id>', SkillAPI.as_view(), name='skill_api'),
+    path('skills/<int:id>', SkillAPI.as_view(), name='skill_api'),
     # Api Post the Skill
-    path('skill/', SkillAPI.as_view(), name='skill_post'),
+    path('skills/create/', SkillAPI.as_view(), name='skill_post'),
 ]

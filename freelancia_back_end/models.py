e0ae04c1dc4f8c9cfbe388a41617e0b695f2f041
AbdAlla-AboElMagd <abd.moh1990@gmail.com>
2025-03-19 21:45:20 +0200

    Changing the default permission

--- freelancia_back_end/models.py
+++ freelancia_back_end/models.py
@@ -130,6 +130,10 @@ class Proposal(models.Model):
     def __str__(self):
         return f'{self.user.username} - {self.project.project_name}'
 
+class ProposalImage(models.Model):
+    Proposal = models.ForeignKey(Proposal, on_delete=models.CASCADE , related_name='images')
+    image = models.ImageField()
+    
 
 class BlackListedToken(models.Model):
     token = models.CharField(max_length=500)
